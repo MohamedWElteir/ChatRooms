6b255e88db6fb28c445f01c93d85b861b71d4fb3
Mohamed Wael <hfhybujj@gmail.com>
2026-01-19 18:41:38 +0200

    Add Blazor UI project with reference to only the API

--- ChatRooms.AppHost/AppHost.cs
+++ ChatRooms.AppHost/AppHost.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ChatRooms_API>("chatrooms-api");
 
+builder.AddProject<Projects.ChatRooms_Blazor>("chatrooms-blazor");
+
 builder.Build().Run();
