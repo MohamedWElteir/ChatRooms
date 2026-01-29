764d89d3772998bce19b6ae6d19b8ea67599eff7
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2026-01-29 18:14:59 +0200

    #10

--- ChatRooms.Domain/Messages/Content.cs
+++ ChatRooms.Domain/Messages/Content.cs
@@ -4,21 +4,21 @@ using System.Text;
 
 namespace ChatRooms.Domain.Messages
 {
-    public readonly record struct Description
+    public readonly record struct Content
     {
         public string Value { get; }
         private const int MIN_LENGTH = 0;
 
-        private Description(string value)
+        private Content(string value)
         {
             if (string.IsNullOrWhiteSpace(value))
-                throw new ArgumentException("Description cannot be null or whitespace.", nameof(value));
+                throw new ArgumentException("Content cannot be null or whitespace.", nameof(value));
             if (value.Length == MIN_LENGTH)
-                throw new ArgumentException($"Description must be greater than zero.", nameof(value));
+                throw new ArgumentException($"Content must be greater than zero.", nameof(value));
             Value = value;
         }
 
-        public static Description Create(string value) => new(value);
+        public static Content Create(string value) => new(value);
         public override string ToString() => Value;
     }
 }
